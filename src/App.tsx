fbb0c239cd8ecc5d0bfe8c71dcdcf9e2c42d9dc8
Sumit Kumar <Sumit.SHARMA@3ds.com>
2023-07-27 12:16:27 +0530

    added query initial config

--- src/App.tsx
+++ src/App.tsx
@@ -6,17 +6,24 @@ import GlobalStyle from './theme/GlobalStyle';
 import { createTheme, ThemeProvider } from '@mui/material'
 import { typography } from './theme/typography';
 import './app.css'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+
+const queryClient = new QueryClient()
 
 function App() {
 
-  const theme = createTheme({typography})
+  const theme = createTheme({ typography })
 
   return (
     <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <BrowserRouter>
-        <Router />
-      </BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        <GlobalStyle />
+        <BrowserRouter>
+          <Router />
+        </BrowserRouter>
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
     </ThemeProvider>
   )
 }
