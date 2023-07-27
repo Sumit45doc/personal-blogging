eab4c084ac58de321d018a811a54b1fc7ebc70c3
Sumit Kumar <Sumit.SHARMA@3ds.com>
2023-07-27 12:15:45 +0530

    config axios

---
+++ src/api.ts
@@ -0,0 +1,22 @@
+import axios from "axios";
+import { api_constant } from "./state/api_constant";
+import { localStorageConstant } from "./state/localstorage_constant";
+import { get_popular_posts } from "./state/response_constant";
+
+export const API = axios.create({
+    baseURL: `${import.meta.env.VITE_API_BASE_URL}`
+});
+
+console.log(import.meta.env.VITE_API_BASE_URL)
+
+API.interceptors.request.use((req) => {
+    const token = localStorage.getItem(localStorageConstant.AUTHTOKEN)
+    if (token) {
+        req.headers['x-auth-token'] = `Bearer ${JSON.parse(token)}`;
+    }
+
+    return req;
+});
+
+
+export const getPopularPosts = () => API.get<get_popular_posts>(api_constant.getPopularPosts)
