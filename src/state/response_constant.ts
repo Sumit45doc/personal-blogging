a1d7c663988d0fc3390cfe194eaec5147f7ae07d
Sumit Kumar <Sumit.SHARMA@3ds.com>
2023-08-01 00:12:00 +0530

    updated response const getpopularpost

--- src/state/response_constant.ts
+++ src/state/response_constant.ts
@@ -2,11 +2,16 @@ export type api_error = {
     error: string
 }
 
-export type get_popular_posts = {
+export type get_popular_post = {
     title: string,
     _id: string,
     selectedFile: string,
+    description: string,
     type: 'personal' | 'design',
     likes: string[],
-    count: number
-}[]
+    count: number,
+    createdAt: Date,
+    updatedAt: Date
+}
+
+export type get_popular_posts = { data: get_popular_post[] }
