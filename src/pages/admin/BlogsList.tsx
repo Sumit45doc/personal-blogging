47057d3bed82a17d2fa1ad249978329cea745003
Sumit Kumar <Sumit.SHARMA@3ds.com>
2023-08-01 23:00:10 +0530

    blogslist componet defined

---
+++ src/pages/admin/BlogsList.tsx
@@ -0,0 +1,13 @@
+import { Container, Typography } from "@mui/material"
+import BlogsTable from "../../components/sections/admin/blogs-list/BlogsTable"
+
+function BlogsList() {
+  return (
+    <Container maxWidth='lg' sx={{ py: '1rem' }}>
+      <Typography variant="h2" sx={{ fontSize: { xs: '1.5rem', md: '2.2rem' }, textAlign: 'left', my: 2 }}> BLOG LIST</Typography>
+      <BlogsTable />
+    </Container>
+  )
+}
+
+export default BlogsList
