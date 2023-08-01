d1e7f110eddc98f65d6f87d9702fda9d6a471a68
Sumit Kumar <Sumit.SHARMA@3ds.com>
2023-08-01 22:58:57 +0530

    admin blog component defined

--- src/components/shared/BlogForm.tsx
+++ src/components/shared/BlogForm.tsx
@@ -17,7 +17,7 @@ export enum PostType {
 export type BlogFormValue = {
   title: string;
   description: string;
-  type: PostType;
+  type: 'personal' | 'design';
   selectedFile: string
 }
 
@@ -63,142 +63,140 @@ function BlogForm({ initialValues, onSubmit }: Props) {
   }
 
   return (
-    <Box sx={{ m: { xs: '1rem', md: '3rem' } }}>
-      <Formik
-        initialValues={initialValues}
-        validationSchema={validationSchema}
-        onSubmit={onSubmit}
-      >
-        {formik => {
-          return (
-            <>
-              <Form>
-                <Grid container spacing={3}>
-                  <Grid item xs={12} md={8}>
-                    <StyledCard>
-                      <Stack spacing={3}>
-                        <TextField
-                          id="title"
-                          name="title"
-                          label="Title"
-                          value={formik.values.title}
-                          onChange={formik.handleChange}
-                          onBlur={formik.handleBlur}
-                          error={formik.touched.title && Boolean(formik.errors.title)}
-                          helperText={formik.touched.title && formik.errors.title}
-                        />
+    <Formik
+      initialValues={initialValues}
+      validationSchema={validationSchema}
+      onSubmit={onSubmit}
+    >
+      {formik => {
+        return (
+          <>
+            <Form>
+              <Grid container spacing={3}>
+                <Grid item xs={12} md={8}>
+                  <StyledCard>
+                    <Stack spacing={3}>
+                      <TextField
+                        id="title"
+                        name="title"
+                        label="Title"
+                        value={formik.values.title}
+                        onChange={formik.handleChange}
+                        onBlur={formik.handleBlur}
+                        error={formik.touched.title && Boolean(formik.errors.title)}
+                        helperText={formik.touched.title && formik.errors.title}
+                      />
 
-                        <TextField
-                          id="description"
-                          name="description"
-                          label="Description"
-                          multiline
-                          rows={4}
-                          value={formik.values.description}
-                          onChange={formik.handleChange}
-                          onBlur={formik.handleBlur}
-                          error={formik.touched.description && Boolean(formik.errors.description)}
-                          helperText={formik.touched.description && formik.errors.description}
-                        />
+                      <TextField
+                        id="description"
+                        name="description"
+                        label="Description"
+                        multiline
+                        rows={4}
+                        value={formik.values.description}
+                        onChange={formik.handleChange}
+                        onBlur={formik.handleBlur}
+                        error={formik.touched.description && Boolean(formik.errors.description)}
+                        helperText={formik.touched.description && formik.errors.description}
+                      />
 
-                        <RadioGroup
-                          aria-labelledby="demo-controlled-radio-buttons-group"
-                          name="type"
-                          value={formik.values.type}
-                          onChange={formik.handleChange}
-                        >
-                          <FormControlLabel value={PostType.DESIGN} control={<Radio />} label={PostType.DESIGN.toLocaleUpperCase()} />
-                          <FormControlLabel value={PostType.PERSONAL} control={<Radio />} label={PostType.PERSONAL.toLocaleUpperCase()} />
-                        </RadioGroup>
-
+                      <RadioGroup
+                        aria-labelledby="demo-controlled-radio-buttons-group"
+                        name="type"
+                        value={formik.values.type}
+                        onChange={formik.handleChange}
+                      >
+                        <FormControlLabel value={PostType.DESIGN} control={<Radio />} label={PostType.DESIGN.toLocaleUpperCase()} />
+                        <FormControlLabel value={PostType.PERSONAL} control={<Radio />} label={PostType.PERSONAL.toLocaleUpperCase()} />
+                      </RadioGroup>
 
 
-                        <Stack spacing={1}>
-                          <Typography variant="subtitle2" sx={{ color: 'text.secondary' }}>
-                            Cover
-                          </Typography>
 
-                          <TextField
-                            id="upload-blog-image"
-                            type="file"
-                            name='selectedFile'
-                            inputProps={{ accept: 'image/*, .png. jpg. webp' }}
-                            onChange={e => {
-                              const getBase64Img = async () => {
-                                const image = await handleFileRead(e)
-                                if (image) {
-                                  formik.setFieldValue('selectedFile', image)
-                                }
-                              }
-                              getBase64Img()
-                            }}
-                            sx={{ display: 'none' }}
-                            onBlur={formik.handleBlur}
-                          />
-                          <label htmlFor="upload-blog-image">
-                            {formik.values.selectedFile ? (
-                              <Box sx={{ position: 'relative', height: '30vh' }} >
-                                <IconButton
-                                  sx={{
-                                    position: 'absolute',
-                                    zIndex: 10,
-                                    right: 8,
-                                    top: 8
-                                  }}
-                                  onClick={(e) => {
-                                    e.preventDefault()
-                                    removeImage(formik)
-                                  }}
-                                >
-                                  <Close fontSize="medium" />
-                                </IconButton>
-                                <SingleFilePreview file={formik.values.selectedFile} />
-                              </Box>
-                            ) : <UploadPlaceholder />}
-
-                          </label>
-
-                          {<FormHelperText error={formik.touched.selectedFile && Boolean(formik.errors.selectedFile)} sx={{ px: 2 }}>
-                            {formik.touched.selectedFile && formik.errors.selectedFile}
-                          </FormHelperText>
-                          }
-                        </Stack>
-
-                        <LoadingButton type="submit" color="secondary" variant="contained" loading={formik.isSubmitting} disabled={!formik.isValid} >submit</LoadingButton>
-                      </Stack>
-                    </StyledCard>
-                  </Grid>
-
-                  {/* PREVIEW */}
-                  <Grid item xs={12} md={4}>
-                    <StyledCard>
-                      <Stack spacing={3}>
+                      <Stack spacing={1}>
                         <Typography variant="subtitle2" sx={{ color: 'text.secondary' }}>
-                          Preview
+                          Cover
                         </Typography>
-                        <Box sx={{ position: 'relative', height: '40vh' }} >
-                          {formik.values.selectedFile && (
-                            <Box sx={{ cursor: 'pointer' }} onClick={() => setFullImage(true)}>
+
+                        <TextField
+                          id="upload-blog-image"
+                          type="file"
+                          name='selectedFile'
+                          inputProps={{ accept: 'image/*, .png. jpg. webp' }}
+                          onChange={e => {
+                            const getBase64Img = async () => {
+                              const image = await handleFileRead(e)
+                              if (image) {
+                                formik.setFieldValue('selectedFile', image)
+                              }
+                            }
+                            getBase64Img()
+                          }}
+                          sx={{ display: 'none' }}
+                          onBlur={formik.handleBlur}
+                        />
+                        <label htmlFor="upload-blog-image">
+                          {formik.values.selectedFile ? (
+                            <Box sx={{ position: 'relative', height: '30vh' }} >
+                              <IconButton
+                                sx={{
+                                  position: 'absolute',
+                                  zIndex: 10,
+                                  right: 8,
+                                  top: 8
+                                }}
+                                onClick={(e) => {
+                                  e.preventDefault()
+                                  removeImage(formik)
+                                }}
+                              >
+                                <Close fontSize="medium" />
+                              </IconButton>
                               <SingleFilePreview file={formik.values.selectedFile} />
                             </Box>
-                          )}
-                        </Box>
+                          ) : <UploadPlaceholder />}
+
+                        </label>
+
+                        {<FormHelperText error={formik.touched.selectedFile && Boolean(formik.errors.selectedFile)} sx={{ px: 2 }}>
+                          {formik.touched.selectedFile && formik.errors.selectedFile}
+                        </FormHelperText>
+                        }
                       </Stack>
-                    </StyledCard>
-                  </Grid>
-                  
+
+                      <LoadingButton type="submit" color="secondary" variant="contained" loading={formik.isSubmitting} disabled={!formik.isValid} >submit</LoadingButton>
+                    </Stack>
+                  </StyledCard>
                 </Grid>
-                <ImagePopup
-                  image={formik.values.selectedFile}
-                  open={openFullImage}
-                  handleClose={() => setFullImage(false)}
-                />
-              </Form>
-            </>
-          )
-        }}
-      </Formik>
-    </Box >
+
+                {/* PREVIEW */}
+                <Grid item xs={12} md={4}>
+                  <StyledCard>
+                    <Stack spacing={3}>
+                      <Typography variant="subtitle2" sx={{ color: 'text.secondary' }}>
+                        Preview
+                      </Typography>
+                      <Box sx={{ position: 'relative', height: '40vh' }} >
+                        {formik.values.selectedFile && (
+                          <Box sx={{ cursor: 'pointer' }} onClick={() => setFullImage(true)}>
+                            <SingleFilePreview file={formik.values.selectedFile} />
+                          </Box>
+                        )}
+                      </Box>
+                    </Stack>
+                  </StyledCard>
+                </Grid>
+
+              </Grid>
+              <ImagePopup
+                image={formik.values.selectedFile}
+                open={openFullImage}
+                handleClose={() => setFullImage(false)}
+              />
+            </Form>
+          </>
+        )
+      }}
+    </Formik>
   )
 }
 
