bcefefa41f4a1f5458a593748d8aa19260a756ee
Sumit Kumar <Sumit.SHARMA@3ds.com>
2023-08-06 16:53:36 +0530

    added adminheader to bloglist & createblog

--- src/pages/admin/BlogsList.tsx
+++ src/pages/admin/BlogsList.tsx
@@ -1,12 +1,15 @@
 import { Container, Typography } from "@mui/material"
 import BlogsTable from "../../components/sections/admin/blogs-list/BlogsTable"
+import AdminHeader from "../../components/shared/layout/AdminHeader"
 
 function BlogsList() {
   return (
-    <Container maxWidth='lg' sx={{ py: '1rem' }}>
-      <Typography variant="h2" sx={{ fontSize: { xs: '1.5rem', md: '2.2rem' }, textAlign: 'left', my: 2 }}> BLOG LIST</Typography>
-      <BlogsTable />
-    </Container>
+    <AdminHeader>
+      <Container maxWidth='lg' sx={{ py: '1rem' }}>
+        <Typography variant="h2" sx={{ fontSize: { xs: '1.5rem', md: '2.2rem' }, textAlign: 'left', my: 2 }}> BLOG LIST</Typography>
+        <BlogsTable />
+      </Container>
+    </AdminHeader>
   )
 }
 
