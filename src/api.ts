43bf724c785d3ec13c84b19f06a57f03f627c35e
Sumit Kumar <Sumit.SHARMA@3ds.com>
2023-08-06 16:48:24 +0530

    added getlatestpost, createpost, updatepost, deletepost, incrementviewpost, likepost, signin & signup api axios call

--- src/api.ts
+++ src/api.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 import { api_constant } from "./state/api_constant";
 import { localStorageConstant } from "./state/localstorage_constant";
-import { get_popular_posts } from "./state/response_constant";
+import { delete_post, get_latest_posts, get_popular_posts, get_post, get_user, update_like_post, update_view_post } from "./state/response_constant";
+import { post_create_blog, post_signin, post_signup, post_update_blog } from "./state/request_constant";
 
 export const API = axios.create({
     baseURL: `${import.meta.env.VITE_API_BASE_URL}`
@@ -16,5 +17,20 @@ API.interceptors.request.use((req) => {
     return req;
 });
 
-
+// get post
 export const getPopularPosts = () => API.get<get_popular_posts>(api_constant.getPopularPosts)
+export const getLatestPosts = () => API.get<get_latest_posts>(api_constant.getLatestPost)
+
+// CUD operation
+export const createPost = (data: post_create_blog): Promise<get_post> => API.post(api_constant.createPost, data)
+export const updatePost = (id: string, data: post_update_blog): Promise<get_post> => API.post(api_constant.editPost(id), data)
+export const deletePost = (id: string): Promise<delete_post> => API.delete(api_constant.deletePost(id))
+
+// analytics
+export const incrementViewPost = (id: string): Promise<update_view_post> => API.patch(api_constant.incrementViewPost(id))
+export const likePost = (id: string): Promise<update_like_post> => API.patch(api_constant.likePost(id))
+
+
+// auth
+export const signin = (data: post_signin): Promise<get_user> => API.post(api_constant.signin, data)
+export const signup = (data: post_signup): Promise<get_user> => API.post(api_constant.signup, data)
