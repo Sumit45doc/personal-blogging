fb771ecbbb9eb4075d3d6369aedba56b956f476d
Sumit Kumar <Sumit.SHARMA@3ds.com>
2023-08-06 16:52:20 +0530

    added constant types

--- src/state/api_constant.ts
+++ src/state/api_constant.ts
@@ -1,6 +1,17 @@
 
 const POST = '/post'
+const AUTH = '/auth'
 
-export enum api_constant {
-    getPopularPosts = `${POST}/get-popular-post`
+export const api_constant = {
+    getPopularPosts: `${POST}/get-popular-post`,
+    getLatestPost: `${POST}/get-latest-post`,
+    createPost: `${POST}/create`,
+    editPost: (id: string) => `${POST}/update-post/${id}`,
+    deletePost: (id: string) => `${POST}/delete-post/${id}`,
+    incrementViewPost: (id: string) => `${POST}/update-count/${id}`,
+    likePost: (id: string) => `${POST}/like/${id}`,
+
+    // auth
+    signin: `${AUTH}/signin`,
+    signup: `${AUTH}/signup`
 }
