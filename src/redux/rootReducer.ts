abd8981abe620648006eb29ec9a0e8a390996226
Sumit Kumar <Sumit.SHARMA@3ds.com>
2023-08-16 21:21:40 +0530

    added type to reducer

--- src/redux/rootReducer.ts
+++ src/redux/rootReducer.ts
@@ -1,12 +1,13 @@
-import { combineReducers } from 'redux';
+import { Reducer, combineReducers } from 'redux';
 
 // slices
 import userReducer from './slice/user'
+import { get_user } from '../state/response_constant';
 
 // ----------------------------------------------------------------------
 
 const rootReducer = combineReducers({
-  user: userReducer
+  user: userReducer as Reducer<get_user>
 });
 
 export default rootReducer;
