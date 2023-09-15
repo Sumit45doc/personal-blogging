081394dddfd43544d6a8204dcd83ce8ad877783a
Sumit Kumar <Sumit.SHARMA@3ds.com>
2023-09-16 00:43:24 +0530

    formated detail component

--- src/components/sections/home/Detail.tsx
+++ src/components/sections/home/Detail.tsx
@@ -7,13 +7,13 @@ type Props = {
 }
 
 
-function Detail({heroTitle}: Props) {
+function Detail({ heroTitle }: Props) {
 
     return (
         <DetailContainer>
             <TextBox maxWidth='xl'>
                 {!heroTitle && <Typography variant='h6'>What  I Do?</Typography>}
-                <BlogerDescription sx={{ textAlign: !!heroTitle ? 'center' : 'left'  }} variant='h2'>{heroTitle ?? blogerDescription}</BlogerDescription>
+                <BlogerDescription sx={{ textAlign: !!heroTitle ? 'center' : 'left' }} variant='h2'>{heroTitle ?? blogerDescription}</BlogerDescription>
             </TextBox>
         </DetailContainer>
     )
@@ -33,7 +33,7 @@ const TextBox = styled(Box)(() => ({
     overflow: 'hidden'
 }))
 
-const BlogerDescription = styled(Typography)(({theme}) => ({
+const BlogerDescription = styled(Typography)(({ theme }) => ({
     textAlign: 'left',
     [theme.breakpoints.down('md')]: {
         fontSize: '2.2rem',
